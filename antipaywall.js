bcefa831b519be2b7a5fc5c9bd76cb9ad433db67
deka9584 <71175340+deka9584@users.noreply.github.com>
2025-11-16 23:12:39 +0100

    Added support for qiota and cookie wall

--- antipaywall.js
+++ antipaywall.js
@@ -1,25 +1,29 @@
 //Remove paywalled content from some websites
 //Bookmark: javascript:[PASTE FUNCTION]
 
-(function antiPaywall(d, pm) {
-    pm = pm || d.querySelector(".paywall_message");
-    if (pm) {
-        pm.remove();
-    }
+(function (document, fetch, Swal, DOMParser) {
+    function restorePwContents () {
+        var intro = document.querySelector(".paywall_intro");
 
-    Swal.close();
+        if (!intro) {
+            return;
+        }
+        
+        var pwm = document.querySelector(".paywall_message");
 
-    fetch(window.location.href)
-        .then(function (r) {
-            return r.text();
-        })
-        .then(function (html) {
-            var parser = new DOMParser();
-            var parsedDoc = parser.parseFromString(html, 'text/html');
-            var contents = parsedDoc.querySelectorAll(".paywalled_content");
-            var intro = d.querySelector(".paywall_intro");
+        if (pwm) {
+            pwm.remove();
+        }
 
-            if (intro) {
+        fetch(window.location.href)
+            .then(function (r) {
+                return r.text();
+            })
+            .then(function (html) {
+                var parser = new DOMParser();
+                var parsedDoc = parser.parseFromString(html, 'text/html');
+                var contents = parsedDoc.querySelectorAll(".paywalled_content");
+                
                 for (var i = 0; i < contents.length; i++) {
                     var cont = contents[i];
                     cont.style.display = "block";
@@ -27,6 +31,33 @@
                 }
 
                 intro.style.display = "none";
-            }
+            });
+    }
+
+    function qiotaBypass () {
+        document.querySelectorAll(".qiota_reserve").forEach(el => {
+            el.style.removeProperty("height");
+            el.style.removeProperty("overflow");
         });
-})(window.document);
+    }
+
+    function closeCookieOverlay () {
+        const style = document.createElement("style");
+        style.textContent = ".cookiewall, .privacy-cp-wall {display:none!important; opacity:0!important; visibility:hidden!important; pointer-events:none!important;}";
+        document.head.append(style);
+
+        if (Swal && typeof Swal.close === "function") {
+            Swal.close();
+        }
+
+        document.querySelectorAll(".privacy-cp-wall, .iubenda-cs-banner").forEach(function (el) {
+            el.remove();
+        });
+
+        document.body.classList.remove("noScroll");
+    }
+
+    restorePwContents();
+    qiotaBypass();
+    closeCookieOverlay();
+})(window.document, window.fetch, window.Swal, window.DOMParser);
